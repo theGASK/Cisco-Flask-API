df245a24ca82498645ed781efab324222382f515
G.A. Keller <83971217+theGASK@users.noreply.github.com>
2021-06-20 22:53:30 +0100

    parse_args working

--- app_video.py
+++ app_video.py
@@ -7,6 +7,10 @@ app  =Flask(__name__)
 api = Api(app)
 
 video_put_args = reqparse.RequestParser()
+video_put_args.add_argument('room', type=str, help='Name of the room')
+video_put_args.add_argument('participant', type=str, help='Name of the participant')
+video_put_args.add_argument('spectators', type=int, help='Number of spectators')
+
 videos = {}
 
 class Video_Stream(Resource):
@@ -14,8 +18,8 @@ class Video_Stream(Resource):
         return videos[video_id]
     
     def put(self, video_id):
-
-        return
+        args = video_put_args.parse_args()
+        return {video_id: args}
 
 
 api.add_resource(Video_Stream, "/Video_Streaming/<int:video_id>")
